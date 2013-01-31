9bd2f9e6b3a25d37ac35047b2af12515e743519a
Drew Erny <drewerny@gmail.com>
2013-01-30 23:53:04 -0600

    fixed linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -1,68 +1,73 @@
 from node import Node
 
 class LinkedList:
-	"""A Python3 class for linked lists
-
-	full range of operations supported
-	"""
 
 	def __init__(self,value = None):
-		self._headNode = Node(value,None)
-		self._currentNode = self._topNode
-		self._tailNode = self._tailNode
-		self._size = 1
-
-	def head(self,value = None):
-		if(value = None):
-			return self._currentNode.head()
+		# if no value is passed in
+		if(value == None):
+			self._size = 0
+			self._headNode = None
+			self._tailNode = None
 		else:
-			self._currentNode.setHead(value)
+			self._headNode = Node(value,None)
+			self._tailNode = self._headNode
+			self._size = 1
 
-	def tail(self):
-		self._currentNode = self.currentNode.tail()
-
-	def top(self):
-		self._currentNode = self._headNode
-
-	def bottom(self):
-		self._currentNode = self._tailNode
+	def get(self,index):
+		if(index == 0):
+			return self._headNode.head()
+		elif(index == self._size - 1):
+			return self._tailNode.head()
+		else:
+			i = 0
+			walk = self._headNode
+			while(i < index):
+				walk.tail()
+			return walk.head()
 
 	def frontadd(self,value):
-		if(self._headNode.head() == None):
-			self._headNode = Node(value,None)
-		else:
-			self._headNode = Node(value,self._headNode)
+		self._headNode = Node(value,self._headNode)
+		# if the headNode has tail None (is the last node)
+		# then it IS tailNode
+		if(self._headNode.tail() == None):
+			self._tailNode = self._headNode
+		self._size += 1
 
 	def backadd(self,value):
-		self._bottomNode.setTail(Node(value,None))
-		self._bottomNode = self._bottomNode.tail()
-
-	def set(self,value,index):
-		i = 0
-		while(i <= index):
-			self.tail()
-			i += 1
-		self.head(value)
-		self.top()
-
-	def indexadd(self,value,index):
-		i = 0
-		while(i <= index)
-			self.tail()
-		self._currentNode().setTail() = Node(value,self._currentNode.tail())
-		self.top()
-
+		if(self._headNode == None):
+			self.frontadd(value)
+			return
+		self._tailNode.setTail(Node(value,None))
+		self._tailNode = self._tailNode.tail()
+		self._size += 1
 
 	def frontremove(self):
-		rval = self._topNode.head()
-		self._topNode = self._topNode.tail()
+		rval = self._headNode.head()
+		self._headNode = self._headNode.tail()
+		self._size -= 1
 		return rval
 
 	def backremove(self):
-		rval = self._bottomNode.head()
-		while(self._currentNode.tail() != self._bottomNode)
-			self.tail()
-		self._bottomNode = self._currentNode
-		self._bottomNode.setTail(None)
+		rval = self._tailNode.head()
+		walk = self._headNode()
 
-		self.top()
+		while(walk.tail() != self._tailNode):
+			walk = walk.tail()
+
+		self._tailNode = walk
+		self._tailNode.setTail(None)
+		self._size -= 1
+
+		return rval
+
+	def extract(self):
+		if(self._headNode == None):
+			return []
+		rlist = []
+		walk = self._headNode
+		while(walk.tail() != None):
+			rlist.append(walk.head())
+			walk = walk.tail()
+		rlist.append(walk.head())
+
+		return rlist
